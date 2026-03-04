e3ab450de292eb79aa8dea2090872cceffd3f9d5
M.H SHAGOR <srq001100@gmail.com>
2026-03-05 04:18:41 +0600

    update

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -9,50 +9,24 @@ class PackageServiceProvider extends ServiceProvider
 {
     protected $basePath = __DIR__ . '/../../';
 
-    protected $packages = [
-        'file-picker',
-    ];
-    
-    private function publishPackage($package)
-    {
-        if($package === 'all') {
-            $this->publishAll();
-            return;
-        }
-        $paths = match($package) {
-            'file-picker' => $this->publishFilePicker($package),
-            
-            default => throw new \Exception("Unknown package: {$package}"),
-        };
-        
-        $this->publishes($paths, $package);
-    }
+    private const TAG_COMPONENTS = 'file-picker';
 
-    private function publishAll()
-    {
-        $paths = [
-            ...$this->publishFilePicker('file-picker'),
-        ];
-        
-        $this->publishes($paths, 'all');
-    }
-
-    private function publishFilePicker($package)
+    private function publishPaths(): array
     {
         return [
-            $this->basePath . '/assets/demo/file-picker.html' => resource_path('views/sgd/file-picker.html'),
-            $this->basePath . '/assets/js' => resource_path('js/sgd'),
-            $this->basePath . '/assets/css' => resource_path('css/sgd'),
-            $this->basePath . '/assets/components' => resource_path('views/components'),
+            $this->basePath . 'assets/demo' => resource_path('views/sgd'),
+            $this->basePath . 'assets/js' => resource_path('js/sgd'),
+            $this->basePath . 'assets/css' => resource_path('css/sgd'),
+            $this->basePath . 'assets/components' => resource_path('views/components'),
         ];
     }
-
+    
 
     public function boot()
     {
-        foreach ($this->packages as $package) {
-            $this->publishPackage($package);
-        }
+        $publishPaths = $this->publishPaths();
+
+        $this->publishes($publishPaths, self::TAG_COMPONENTS);
     }
     
     public function register()
